da3749a4a676690eb2f5bcc63f1e80278d8e1af4
UBnkantah <ubongnkantahuwem@gmail.com>
2025-12-08 14:51:41 +0100

    update

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const connectMongoDB = require("./res/config");
+// const connectMongoDB = require("./res/config");
 const appRouter = require("./res/authrouter");
 const app = express()
 const port = 5000
 
+const mongoose = require("mongoose");
+// mongodb+srv://mindversityadmin:admin2025@mindversity.shz2nxg.mongodb.net/?appName=mindversity
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://mindversityadmin:mindversity2025@cluster0.wchyqfm.mongodb.net");
+    console.log("Database Connected");
+  } catch (err) {
+    console.log(`Database connection error, ${err}`);
+  }
+};
+connectMongoDB();
+
 require("dotenv").config();
 
 app.use(cors());
-connectMongoDB();
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(
--- res/config.js
+++ res/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 // mongodb+srv://mindversityadmin:admin2025@mindversity.shz2nxg.mongodb.net/?appName=mindversity
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://mindversityadmin:mindversity2025@cluster0.wchyqfm.mongodb.net/");
+    await mongoose.connect("mongodb+srv://mindversityadmin:mindversity2025@cluster0.wchyqfm.mongodb.net");
     console.log("Database Connected");
   } catch (err) {
     console.log(`Database connection error, ${err}`);
