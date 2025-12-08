9049e8c490107a71bef47d2b44706711f39a70a2
UBnkantah <ubongnkantahuwem@gmail.com>
2025-12-08 12:41:57 +0100

    update

--- res/config.js
+++ res/config.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+// mongodb+srv://mindversityadmin:admin2025@mindversity.shz2nxg.mongodb.net/?appName=mindversity
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL || "mongodb+srv://mindversityadmin:admin2025@mindversity.shz2nxg.mongodb.net/?appName=mindversity");
+    await mongoose.connect(process.env.DATABASE_URL || "mongodb+srv://mindversityadmin:mindversity2025@cluster0.wchyqfm.mongodb.net/");
     console.log("Database Connected");
   } catch (err) {
     console.log(`Database connection error, ${err}`);
